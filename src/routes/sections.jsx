d97e5fc9e761c8c009ec02bbb33c8c0eb43a6109
wipa-m <wipawadee.mk@gmail.com>
2024-03-28 01:38:22 +0700

    Remove unused file

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -4,10 +4,7 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
-export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 export const MovieFinder = lazy(() => import('src/pages/movie-finder'));
 export const MovieDetail = lazy(() => import('src/pages/movie-detail'));
@@ -27,8 +24,6 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
-        { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
         { path: 'movie-finder', element: <MovieFinder /> },
         { path: 'movie-detail/:id', element: <MovieDetail /> },
         { path: 'my-favorite', element: <MyFavorite /> },
